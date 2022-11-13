e1932eab2cb214781a538e87141664e7ca9da0b8
Sergey Sorokin <mumea@mail.ru>
2022-11-13 14:09:41 +0300

    Сделал вывод характеристик покемона через метод

--- Program.cs
+++ Program.cs
@@ -4,15 +4,20 @@
     {
         static void Main()
         {
-            Pokemon pokemon1 = new Pokemon(50, 15, 41, 35);
-            Pokemon pokemon2 = new Pokemon(50, 10, 56, 69);
+            Pokemon pokemon1 = new Pokemon("Покемон1", 50, 15, 41, 35);
+            Pokemon pokemon2 = new Pokemon("Покемон2",50, 10, 56, 69);
             Console.WriteLine("Бой.Pokemon1 vs Pokemon2");
-            Console.WriteLine($"Покемон 1. Базовые характеристики: Здоровье - {pokemon1.Health}, Атака - {pokemon1.Attack}, Броня - {pokemon1.Armor}, Ловкость - {pokemon1.Dexterity}.");
-            Console.WriteLine($"Покемон 2. Базовые характеристики: Здоровье - {pokemon2.Health}, Атака - {pokemon2.Attack}, Броня - {pokemon2.Armor}, Ловкость - {pokemon2.Dexterity}.");
+            WritePokemonStats(pokemon1);
+            WritePokemonStats(pokemon2);
+
+            //Console.WriteLine($"Покемон 1. Базовые характеристики: Здоровье - {pokemon1.Health}, Атака - {pokemon1.Attack}, Броня - {pokemon1.Armor}, Ловкость - {pokemon1.Dexterity}.");
+            //Console.WriteLine($"Покемон 2. Базовые характеристики: Здоровье - {pokemon2.Health}, Атака - {pokemon2.Attack}, Броня - {pokemon2.Armor}, Ловкость - {pokemon2.Dexterity}.");
             Console.WriteLine("Right!");
             Console.WriteLine("Введите команду:");
 
-            string order = Console.ReadLine(); 
+           
+
+                string order = Console.ReadLine(); 
            
             while (order != "Бой окончен")
             {
@@ -77,21 +82,27 @@
             
 
         }
+        static void WritePokemonStats(Pokemon pokemon)
+        {
+            Console.WriteLine($"{pokemon.Name}. Базовые характеристики: Здоровье - {pokemon.Health}, Атака - {pokemon.Attack}, Броня - {pokemon.Armor}, Ловкость - {pokemon.Dexterity}.");
+        }
     }
 
     internal class Pokemon
     {
+        public string Name;
         public int Health;
         public int Attack;
         public int Armor;
         public int Dexterity;
 
-        public Pokemon(int health, int attack, int armor, int dexterity)
+        public Pokemon(string name, int health, int attack, int armor, int dexterity)
         {
             Health = health;
             Attack = attack;
             Armor = armor;
             Dexterity = dexterity;
+            Name = name;
         }
 
         public void attack(Pokemon otherPokemon)
